c2825dde283b7615a189fa0197e35eb2997dc887
Cristian Caroli <cristiancaroli@Cristians-MacBook-Pro.local>
2017-05-10 16:02:42 +0200

    about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,3 +1,39 @@
 import React from 'react'
 
-export const About = props => <div>About!</div>
+export const About = props => (
+  <div>
+    <h2>About This Project</h2>
+
+    <p>
+      All and all,
+      {' '}
+      <em>First Draft Endings</em>
+      {' '}
+      is a project conceived by me, Cristian E. Caroli.
+    </p>
+
+    <p>
+      I follow a fixed set of rules:
+    </p>
+    <ul>
+      <li>It's has to end the story.</li>
+      <li>A single page.</li>
+      <li>No outside feedback before posting.</li>
+      <li>No duplicates. Once a story gets a FDE, it won't get another one.</li>
+      <li>No TV.</li>
+      <li>A % of the script comes from the original.</li>
+    </ul>
+
+    <p>
+      Enjoy.
+    </p>
+
+    <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+      <img
+        alt="Licencia Creative Commons"
+        style={{ borderWidth: 0 }}
+        src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png"
+      />
+    </a>
+  </div>
+)
