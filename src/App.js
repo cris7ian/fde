4d5318008594e4ef8002aa8f9640937ce3645bb8
Cristian Caroli <cristiancaroli@Cristians-MacBook-Pro.local>
2017-05-10 14:47:46 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Home } from './containers/Home'
+import { About } from './containers/About'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="container">
-          <h1>First Draft Endings</h1>
-          <navigation>
-            <a>Home</a> / <a>About</a> / <a>Contact</a>
-          </navigation>
+      <Router>
+        <div>
+          <div className="container">
+            <h1>First Draft Endings</h1>
+            <navigation>
+              <Link to="/">Home</Link>
+              {' '}
+              /
+              {' '}
+              <Link to="/about">About</Link>
+              {' '}
+              /
+              {' '}
+              <a href="https://twitter.com/cris7ian/status/393489939955847169">
+                Contact
+              </a>
+            </navigation>
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </div>
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
