2d2f3da03882489fce93a72076bdfdafbd101b9c
Cristian Caroli <cristiancaroli@Cristians-MacBook-Pro.local>
2017-05-10 14:10:08 +0200

    first template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="container">
+          <h1>First Draft Endings</h1>
+          <navigation>
+            <a>Home</a> / <a>About</a> / <a>Contact</a>
+          </navigation>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     )
   }
