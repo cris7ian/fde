036eee0a4ec698f7d610ea5e1cdd629e60dd8500
Cristian Caroli <cristiancaroli@Cristians-MacBook-Pro.local>
2017-05-23 18:21:25 +0200

    now 1.0.1 for real

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import TweetEmbed from 'react-tweet-embed'
+import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 
-export const Contact = props => (
-  <div>
-    <Helmet>
-      <title>First Draft Endings: Contact</title>
-    </Helmet>
-    <h2>Contact <span role="img" aria-label="hand">👋</span></h2>
-    <TweetEmbed id="393489939955847169" />
-  </div>
-)
+export class Contact extends Component {
+  componentDidMount() {
+    window.twttr.widgets.load()
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>First Draft Endings: Contact</title>
+        </Helmet>
+        <h2>Contact <span role="img" aria-label="hand">👋</span></h2>
+        <blockquote className="twitter-tweet" data-lang="en">
+          <p lang="no" dir="ltr">Hello, stranger.</p>
+          — Cristian™
+          {' '}
+          <span role="img" aria-label="logo">🍥</span>
+          {' '}
+          (@cris7ian)
+          {' '}
+          <a href="https://twitter.com/cris7ian/status/393489939955847169">
+            October 24, 2013
+          </a>
+        </blockquote>
+      </div>
+    )
+  }
+}
